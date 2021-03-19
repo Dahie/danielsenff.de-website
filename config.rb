c700d22b2c87b8f7e04020f79d1633a29c638eef
Daniel Senff <mail@danielsenff.de>
2021-03-19 17:44:17 +0100

    update export of haml to html5

--- config.rb
+++ config.rb
@@ -2,6 +2,8 @@ activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
 
+set :haml, { :format => :html5 }
+
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
